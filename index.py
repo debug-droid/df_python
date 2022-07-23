a64f4416b620745bb7915987923e26ef32e9663e
Diego Augusto <DiegoprogBr@hotmail.com>
2022-07-23 19:27:15 -0300

    python dataframe

---
+++ index.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Diego\Desktop\python\notas_alunos.csv")
+
+df.insert(4, 'media', [7.0,6.5,6.5,8.0])
+
+df.insert(5, 'situacao', ["APROVADO","REPROVADO","REPROVADO","APROVADO"])
+print(df)
+
+maior_falta = df["faltas"].max()
+print("O maior número de faltas foi: " + str(maior_falta))
+
+media_alunos = df["media"].median()
+print("Média geral das notas foi: " + str(media_alunos))
+
+maior_media = df["media"].max()
+print("A maior média é: " + str(maior_media))
